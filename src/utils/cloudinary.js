c25aacd57611a964fa0c6c229bc5518cba239b43
narwadenilesh <nileshnarwade16@gmail.com>
2025-04-26 22:48:16 +0530

    Committing my changes before cleaning history

---
+++ src/utils/cloudinary.js
@@ -0,0 +1,16 @@
+import { v2 as cloudinary} from "cloudinary";
+import fs from "fs"
+
+
+import { v2 as cloudinary } from 'cloudinary';
+
+(async function() {
+
+    // Configuration
+    cloudinary.config({ 
+        cloud_name: 'dyupnpued', 
+        api_key: '915585412842883', 
+        api_secret: '3xHYrahCeXutL57067S' // Click 'View API Keys' above to copy your API secret
+    });
+       
+});
