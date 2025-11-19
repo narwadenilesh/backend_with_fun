574df58d4e44c687e9a093713c1c4d2284af3147
narwadenilesh <nileshnarwade16@gmail.com>
2025-11-19 12:24:33 +0530

    rouets for user controller

--- src/routes/userRoutes.js
+++ src/routes/userRoutes.js
@@ -1,5 +1,17 @@
 import { Router } from "express";
-import { registerUser, loginUser , logoutUser, refreshAccessToken, changeCurrentPassword} from "../controllers/userController.js";
+import { registerUser, 
+    loginUser , 
+    logoutUser, 
+    refreshAccessToken, 
+    changeCurrentPassword,
+    getCurrentUser,
+    updateAccountDetails,
+    updateUserAvatar,
+    updateUserCoverImage,
+    getUserChannelProfile,
+    getWatchHistory
+
+} from "../controllers/userController.js";
 import {upload} from "../middlewares/multer.middleware.js"
 import { verifyJWT } from "../middlewares/auth.middleware.js";
 
